af7f0e8c32d5bf2a03943a93801617617b05527b
Adit A Pillai <aditapillai@gmail.com>
2018-11-23 08:20:18 +0530

    (refactor) : use spring boot webflux

--- src/main/java/com/personal/projects/videostreaming/controllers/ApplicationController.java
+++ src/main/java/com/personal/projects/videostreaming/controllers/ApplicationController.java
@@ -1,20 +1,23 @@
 package com.personal.projects.videostreaming.controllers;
 
-import org.apache.commons.io.IOUtils;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.MediaTypeFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
 @RestController
 public class ApplicationController {
     @GetMapping("video/{fileName}")
-    public void streamVideoFile(@PathVariable String fileName , HttpServletResponse response) throws IOException {
-        response.setContentType("video/mp4");
-        ClassPathResource file = new ClassPathResource(fileName);
-        IOUtils.copy(file.getInputStream(), response.getOutputStream());
+    public ResponseEntity<Resource> streamVideoFile(@PathVariable String fileName) {
+        Resource file = new ClassPathResource(fileName);
+        return ResponseEntity.status(HttpStatus.PARTIAL_CONTENT)
+                             .contentType(MediaTypeFactory.getMediaType(file)
+                                                          .orElse(MediaType.APPLICATION_OCTET_STREAM))
+                             .body(file);
     }
 }
